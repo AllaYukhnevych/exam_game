47d4c9e44b5c9ad255db7a6f40960428f50b15a5
Alla Yukhnevych <allauhnevich25@gmail.com>
2021-11-03 21:34:58 +0200

    Створено класи Session та GameManager

---
+++ exam_game/GameManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Session.h"
+#include <list>
+
+class GameManager 
+{
+public:
+	std::list<Session> GameSessions;
+
+	void PerformGameSession();
+
+	void AddRank(Team& winnerTeam);
+
+	void RemoveRank(Team& looserTeam);
+
+};
+
